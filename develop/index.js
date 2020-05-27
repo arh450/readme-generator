d2d1a4d1ccbcfb47f97c847e54f124ffa6dee23f
hank amadeus <hankamadeus@email.com>
2020-05-27 13:44:50 -0400

    ran npm init and wrote basic outline for questions and markdown format

--- develop/index.js
+++ develop/index.js
@@ -1,8 +1,29 @@
+// Step one npm init - DONE
+
+// The user will be prompted for their GitHub username and other information pertaining to the project the README is for.
+
+// README Requirements
+// At least one badge
 const questions = [
+
+    // What is your project title? "string"
+    // What is your project description? "string"
+    // What is the sequencing of your Table of Contents? -> Generate markdown not a question
+    // What are the installation steps for your project? "string"
+    // What is the usage for your project? "string"
+    // What is your project license? [Array]: "string"
+    // Who are the contributors for your project? "string"
+    // What are your tests? -> prompt user to enter three screenshot images and then format markdown
+    // What are your questions? to be featured as a Contact section for README (see below)
+    // What is your github username? "string"
+    // what is your github email address? "string"
+
 
 ];
 
 function writeToFile(fileName, data) {
+    // Create a .md file with fileName
+    // Write data to fileName
 }
 
 function init() {
